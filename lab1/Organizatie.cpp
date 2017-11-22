99d4a7c229fd7d412b50ebae530d29e8c327f51f
KenJi <dan.baltag.9878@gmail.com>
2017-11-22 12:34:48 +0200

    lab1

--- lab1/Organizatie.cpp
+++ lab1/Organizatie.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 class Organizatie{
@@ -13,22 +14,24 @@ class Organizatie{
 			cout<<"nr_angajati:";cin>>nr_angajati;
 		}
 		void afisare(){
+			cout<<endl;
 			cout<<"denumirea: "<<denumirea<<endl;
 			cout<<"adresa: "<<adresa<<endl;
 			cout<<"numele_directorului: "<<numele_directorului<<endl;
 			cout<<"nr_angajati: "<<nr_angajati<<endl;
+			cout<<endl;
 		}
 		int get_angajati(){
 			return nr_angajati;
 		}
-		int get_denumirea(){
+		string get_denumirea(){
 			return denumirea;
 		}
-		int get_adresa(){
+		string get_adresa(){
 			return adresa;
 		}
 		void scrie_fisier(){
-			ofstream f("Organizatie.txt");
+			ofstream f("Organizatie.txt", ofstream::out | ofstream::app);
 			f<<denumirea<<"\n";
 			f<<adresa<<"\n";
 			f<<numele_directorului<<"\n";
@@ -42,13 +45,13 @@ class Organizatie{
 					cerr<<"eroare la deschiderea fisierului\n";
 					return;
 			}
-			k = 0
-			while (getline(f, linie))
-				if(k++>=n)
-		 			cout<<linie<<"\n";
+			int k = 0;
+			for(int i=0;i<n*4-4;i++)
+				getline(f, linie);
+		 	while(getline(f,s)) cout<<linie<<"\n";
 			f.close();
 		}
-}
+};
 
 void sort(int n, Organizatie tab[]){
 	for(int i=0;i<n-1;i++)
@@ -62,21 +65,24 @@ void sort(int n, Organizatie tab[]){
 
 void afis_custom(int n, Organizatie tab[]){
 	for(int i=0;i<n;i++)
-		if(tab[i].get_denumirea())
+		if(tab[i].get_denumirea() != "")
 			tab[i].afisare();
 }
 
 void adaugare(int* n, Organizatie tab[]){
 	if(*n >= 10) return;
 	*n+=1;
-	for(int i=*n-1;i>0;i--)
+	int i;
+	for(i=*n-1;i>0;i--)
 		tab[i] = tab[i-1];
 	tab[i].citire();
 }
 
-void mod(int n, Organizatie tab[], string adresa){
+void mod(int n, Organizatie tab[]){
+	string x;
+	cout<<"dati adresa:";cin>>x;
 	for(int i=0;i<n;i++)
-		if(tab[i].get_adresa() == adresa)
+		if(tab[i].get_adresa() == x)
 			tab[i].citire();
 }
 
@@ -84,12 +90,13 @@ void s_fisier(int n, Organizatie tab[]){
 	for(int i=0;i<n;i++)
 		tab[i].scrie_fisier();
 }
-void c_fisier(Organizatie tab[]){
-	tab[0].citeste_fisier();
+void c_fisier(int n,Organizatie tab[]){
+	tab[0].citeste_fisier(n);
 }
 
-void c1(int n, Organizatie t[]){
-	for(int i;i<n;i++)
+void c1(int *n, Organizatie t[]){
+	cout<<"nr elemente:";cin>>*n;
+	for(int i;i<*n;i++)
 		t[i].citire();
 }
 
@@ -102,17 +109,16 @@ int main(){
 	int c, n;
 	Organizatie t[10];
 	do{
-		cout<<"1:citirea elementelor\n2:afisarea elementelor\n3:sortarea elementelor\n4:afisarea elementelor pentru care se stie denumirea\n5:adaugarea pe I pozitie a unui element nou\n6:modificarea elementelor pentru o adresa data\n7:scrierea in fisier\8:afisarea ultimului element din fisier\n"; cin>>c;
+		cout<<"1:citirea elementelor\n2:afisarea elementelor\n3:sortarea elementelor\n4:afisarea elementelor pentru care se stie denumirea\n5:adaugarea pe I pozitie a unui element nou\n6:modificarea elementelor pentru o adresa data\n7:scrierea in fisier\n8:afisarea ultimului element din fisier\n"; cin>>c;
 		switch(c){
-			case 1: c1(n,t);
-			case 2: c2(n,t);
-			case 3: sort(n,t);
-			case 4: afis_custom(n,t);
-			case 5: adaugare(n,t);
-			case 6: mod(n,t);
-			case 7: s_fisier(n,t);
-			case 8: c_fisier(n);
-			default:
-		}while(c>0&&c<8)
-	}
+			case 1: c1(&n,t); break;
+			case 2: c2(n,t); break;
+			case 3: sort(n,t); break;
+			case 4: afis_custom(n,t); break;
+			case 5: adaugare(&n,t); break;
+			case 6: mod(n,t); break;
+			case 7: s_fisier(n,t); break;
+			case 8: c_fisier(n,t); break;
+		}
+	}while(c>0&&c<8);
 }
